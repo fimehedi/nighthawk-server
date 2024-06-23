7c869524ace20287e6296a7c710b77e1a2a622e1
FIMehedi <fokrulislammehedi2000@gmail.com>
2024-06-23 14:10:18 +0600

    search api convert to prisma

--- src/modules/common/common.service.mjs
+++ src/modules/common/common.service.mjs
@@ -1,31 +1,61 @@
-import { Asset } from "../../models/asset/asset.model.mjs";
-import { Category } from "../../models/category/category.model.mjs";
-import { SubCategory } from "../../models/sub-category/sub.category.model.mjs";
+import { prisma } from '../../db/prisma.mjs';
 
 class CommonService {
 	async search(searchTerm) {
-
 		// category find by name and short_description
-		const category = await Category.find({
-			$or: [
-				{ name: { $regex: searchTerm, $options: 'i' } },
-				{ short_description: { $regex: searchTerm, $options: 'i' } },
-			],
+		// const category = await Category.find({
+		// 	$or: [
+		// 		{ name: { $regex: searchTerm, $options: 'i' } },
+		// 		{ short_description: { $regex: searchTerm, $options: 'i' } },
+		// 	],
+		// });
+
+		// const subCategory = await SubCategory.find({
+		// 	$or: [
+		// 		{ name: { $regex: searchTerm, $options: 'i' } },
+		// 		{ short_description: { $regex: searchTerm, $options: 'i' } },
+		// 	],
+		// });
+
+		// const asset = await Asset.find({
+		// 	$or: [
+		// 		{ name: { $regex: searchTerm, $options: 'i' } },
+		// 		{ description: { $regex: searchTerm, $options: 'i' } },
+		// 	],
+		// });
+
+		const categoryPromise = prisma.category.findMany({
+			where: {
+				OR: [
+					{ name: { contains: searchTerm, } },
+					{ short_description: { contains: searchTerm, } },
+				],
+			},
 		});
 
-		const subCategory = await SubCategory.find({
-			$or: [
-				{ name: { $regex: searchTerm, $options: 'i' } },
-				{ short_description: { $regex: searchTerm, $options: 'i' } },
-			],
+		const subCategoryPromise = prisma.subCategory.findMany({
+			where: {
+				OR: [
+					{ name: { contains: searchTerm, } },
+					{ short_description: { contains: searchTerm, } },
+				],
+			},
 		});
 
-		const asset = await Asset.find({
-			$or: [
-				{ name: { $regex: searchTerm, $options: 'i' } },
-				{ description: { $regex: searchTerm, $options: 'i' } },
-			],
+		const assetPromise = prisma.asset.findMany({
+			where: {
+				OR: [
+					{ name: { contains: searchTerm, } },
+					{ short_description: { contains: searchTerm, } },
+				],
+			},
 		});
+
+		const [category, subCategory, asset] = await Promise.all([
+			categoryPromise,
+			subCategoryPromise,
+			assetPromise,
+		]);
 
 		return {
 			category,
